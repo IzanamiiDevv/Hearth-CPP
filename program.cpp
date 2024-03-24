c3de5d424cdb3164195d15bbe6023540bf3e1dcb
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-24 16:22:48 +0800

    ternary op

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void hearth(int size){
     std::cout << "Result:" << std::endl;
     std::cout << "---------------------" << std::endl;
-    
+    int n = size%2 ==0?size:size+1;
     std::cout << "---------------------" << std::endl;
 }
 
