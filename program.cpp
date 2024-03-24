d14b76d73d085070728f1ca4ddb48704220cdf19
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-24 16:26:26 +0800

    script

--- program.cpp
+++ program.cpp
@@ -6,11 +6,39 @@ void hearth(int size){
     std::cout << "Result:" << std::endl;
     std::cout << "---------------------" << std::endl;
     int n = size%2 ==0?size:size+1;
+    string str = "";
+    for(int i = n/2;i<n;i+=2){
+        for(int j = 1; j < n - i;j+=2){
+            str += " ";
+        }
+        for(int j = 1; j < i + 1;j++){
+            str += "*";
+        }
+        for(int j = 1; j < n- i + 1;j++){
+            str += " ";
+        }
+        for(int j = 1; j < i + 1;j++){
+            str += "*";
+        }
+        str += "\n";
+    }
+    for(int i = n; i > 0; i--){
+        for(int j = 0; j < n - i;j++){
+            str+=" ";
+            
+        }
+        for(int j = 1; j < i*2;j++){
+            str+="*";
+        }
+        str+="\n";
+    }
+    std::cout << str << std::endl;
     std::cout << "---------------------" << std::endl;
 }
 
 int main(){
     int size;
+    std::cout << "Write the size og the hearth" << std::endl;
     std::cin >> size;
     hearth(size); 
     return 0;
